3814593388d46efdac02cae8fc09a6e4c81446da
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-05-30 22:28:38 +0700

    Vy pushes card stack

---
+++ cardstackdata.js
@@ -0,0 +1,10 @@
+const cardStackData = [
+    {
+            id:1,
+            title: "An Le",
+            address: "123 Le Duan St.",
+            url: "https://picsum.photos/id/11/200/300",
+            price: '115,000,000₫ - 160,000,000₫',
+            duration: '3 months'
+    }
+]
---
+++ components/Home/TBCardStack.tsx
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react'
+import { View, Text, StyleSheet, Dimensions, FlatList, Animated} from 'react-native'
+import CardStackItem from './CardStackItem'
+
+const {width, height} = Dimensions.get('window')
+
+// const infiniteScroll = (dataList: any[]) => {
+//     const numberOfData= dataList.length
+//     let scrollValue = 0, scrolled = 0
+    
+//     setInterval(() => {
+//         scrolled++
+//         if(scrolled < numberOfData){
+//         scrolled= scrollValue + width}
+//         else {[
+//             scrollValue =0,
+//             scrolled =0
+//         ]}
+
+//         // this.flatList.scrollToOffset({animated: true, offset: scrollValue})
+
+//     }, 3000)
+// }
+const TBCardStack = ({data}: any) => {
+    
+    const scrollX = new Animated.Value(0)
+    let position = Animated.divide(scrollX, width)
+    const[dataList, setDataList] = useState(data)
+
+    useEffect(() =>{
+        setDataList(data)
+        // infiniteScroll(dataList)
+    })
+
+    if(data && data.length){
+        return(
+            <View>
+                <FlatList data= {data}
+                // ref={(flatList) => {this.flatList = flatList}}
+                keyExtractor={(item) => item.id} 
+                horizontal
+                pagingEnabled 
+                scrollEnabled 
+                snapToAlignment = 'center' 
+                scrollEventThrottle={16} 
+                decelerationRate = {'fast'}
+                showsHorizontalScrollIndicator = {false}
+                renderItem = {({item: item}) => {return <CardStackItem item = {item} />}
+                }
+
+                // onScroll = {Animated.event([{nativeEvent: {contentOffset: scrollX}}])} 
+
+                />
+                {/* <View style={styles.dotView}>
+                    {data.map((_: any, i: number) => {
+                        let opacity = position.interpolate({
+                            inputRange: [i - 1, i, i + 1],
+                            outputRange: [0.3, 1, 0.3],
+                            extrapolate: 'clamp'})
+                            return(
+                                <Animated.View
+                                key = {i}
+                                style = {[styles.dot, opacity]}
+                                />
+                            )
+                        })}
+                </View> */}
+            </View>
+            
+        )
+    }
+    console.log('Error Occured!')
+    return null
+}
+
+const styles = StyleSheet.create({
+    dotView: {
+        flex: 2,
+        flexDirection: 'row',
+        justifyContent:'center'
+    },
+    dot: {
+        height:10, 
+        width, 
+        backgroundColor:'#595959', 
+        margin: 1, 
+        borderRadius: 5
+    }
+})
+export default TBCardStack
+
