331a658eacc69a4484e3d2fbc47cc69598b6651b
Le Ba Hoang An <36214584+hoanganleba@users.noreply.github.com>
2022-06-07 22:16:17 +0700

    Temporary fix

--- components/Views/Auth/FacebookView.tsx
+++ components/Views/Auth/FacebookView.tsx
@@ -20,7 +20,7 @@ const FacebookView = () => {
       )
       .then((res) => {
         axios
-          .post('http://localhost:3000/api/users/login', {
+          .post('http://localhost:3000/api/auth/login', {
             name: res.data.name,
             email: res.data.email,
             imageUrl: res.data.picture.data.url,
