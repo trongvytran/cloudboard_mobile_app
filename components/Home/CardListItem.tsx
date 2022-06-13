4d3194eb46a4081ae5a2f10f1455beb107629749
An Le1 <an_le1@epam.com>
2022-06-13 15:11:24 +0700

    Modify some code

--- components/Home/CardListItem.tsx
+++ components/Home/CardListItem.tsx
@@ -1,38 +1,43 @@
 import React from 'react'
-import { Pressable, View, StyleSheet, Text, Image, Dimensions } from 'react-native'
+import {
+  Pressable,
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  Dimensions,
+} from 'react-native'
 import Colors from '../../constants/color'
 import DurationBadge from '../UI/DurationBadge'
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'
 const CardListItem = ({ item }: any) => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
   function expensePressHandler() {
     navigation.navigate('BillboardDetailScreen', {
-      item
-    });
+      item,
+    })
   }
   return (
-    <Pressable
-    onPress={expensePressHandler}
-    >
-    <View style={styles.card}>
-      <Image style={styles.image} source={{ uri: item.imageUrl }} />
-      <View style={styles.cardBody}>
-        <View>
-          <Text style={styles.cardName}>{item.name}</Text>
-          <Text style={styles.cardAddress}>{item.address}</Text>
+    <Pressable onPress={expensePressHandler}>
+      <View style={styles.card}>
+        <Image style={styles.image} source={{ uri: item.imageUrl }} />
+        <View style={styles.cardBody}>
+          <View>
+            <Text style={styles.cardName}>{item.name}</Text>
+            <Text style={styles.cardAddress}>{item.address}</Text>
+          </View>
+          <DurationBadge value={item.duration} />
         </View>
-        <DurationBadge value={item.duration} />
+        <Text style={styles.cardPrice}>{item.price}</Text>
       </View>
-      <Text style={styles.cardPrice}>{item.price}</Text>
-    </View>
-  </Pressable>
+    </Pressable>
   )
 }
 export default CardListItem
 
 const styles = StyleSheet.create({
   card: {
-    marginVertical: 16
+    marginVertical: 16,
   },
   image: {
     width: Dimensions.get('window').width / 1.5,
@@ -42,7 +47,7 @@ const styles = StyleSheet.create({
   cardBody: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingTop: 10
+    paddingTop: 10,
   },
   cardName: {
     fontSize: 14,
@@ -51,12 +56,12 @@ const styles = StyleSheet.create({
   cardAddress: {
     color: 'grey',
     fontSize: 12,
-    marginTop: 4
+    marginTop: 4,
   },
   cardPrice: {
     color: Colors.primaryColor,
     fontWeight: '600',
     fontSize: 12,
-    marginTop: 10
+    marginTop: 10,
   },
 })
