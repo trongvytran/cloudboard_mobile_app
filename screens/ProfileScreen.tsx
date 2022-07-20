799d15a67a033930537e172ef633ece2abb4d126
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-07-20 18:58:54 +0700

    Vy pushed for migration

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import { View, FlatList, Text, Pressable, StyleSheet } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import LayoutView from '../components/Views/LayoutView'
 import TitleText from '../components/TitleText'
 import ContainerView from '../components/Views/ContainerView'
@@ -12,6 +12,8 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 // import Ionicons from '@expo/vector-icons/Ionicons'
 import { initializeAsync, logOutAsync } from 'expo-facebook'
+import App from '../App'
+import { clearUserLoginInfo } from '../features/auth/userLoginInfo'
 
 const ProfileScreen = ({ navigation }: any) => {
   const DATA = [
@@ -21,7 +23,19 @@ const ProfileScreen = ({ navigation }: any) => {
   ]
   const [data, setData] = useState([])
 
+  const logOut = async () => {
+    try {
+      await initializeAsync({'appId':'403771287819141', 'appName':'billboard-capstone-project'})
+        await logOutAsync()
+      dispatch(clearUserLoginInfo())
+    }
+    catch(error){
+      console.error(error)
+    }
+  }
+
   const { userLoginInfo } = useSelector((state: any) => state.userLoginInfo)
+  const dispatch = useDispatch()
   const validateUser = userLoginInfo ? (
     <View>
       <ProfileCard
@@ -41,7 +55,8 @@ const ProfileScreen = ({ navigation }: any) => {
         )}
         keyExtractor={(item) => item.id}
       />
-       <Pressable onPress={() => {initializeAsync({'appId':'403771287819141', 'appName':'billboard-capstone-project'}),logOutAsync()}} style={styles.container}>
+       <Pressable onPress={() => logOut()} 
+                                  style={styles.container}>
       <Text style={styles.title}>
        Sign Out
       </Text>
