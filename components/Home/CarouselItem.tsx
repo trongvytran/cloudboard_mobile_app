dba92b37f346cccd9dc98a57fdb73bd3fc577a77
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-08-04 16:24:34 +0700

    Vy fixed Carousel now pressable

--- components/Home/CarouselItem.tsx
+++ components/Home/CarouselItem.tsx
@@ -1,12 +1,19 @@
-import React from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React, { Pressable } from 'react-native'
 import { View, StyleSheet, Text, Image, Dimensions } from 'react-native'
 import Colors from '../../constants/color'
 
 const CarouselItem = ({ item }: any) => {
+  const navigation = useNavigation()
+  const expensePressHandler= async () => {
+    navigation.navigate('BillboardDetailScreen', {
+      item,
+    })}
   return (
-    <View
+    <Pressable
       style={{ width: Dimensions.get('window').width - 32 }}
       className="relative my-4 shadow"
+      onPress={() => expensePressHandler()}
     >
       <Image className="aspect-[8/5] w-full rounded-lg" source={{ uri: item.imageUrl }} />
       <View className="absolute inset-0 rounded-lg bg-gray-900/50"></View>
@@ -14,7 +21,7 @@ const CarouselItem = ({ item }: any) => {
         <Text className="mb-2 text-2xl font-bold tracking-tight text-white">{item.name}</Text>
         <Text className="text-sm font-normal text-white">{item.description}</Text>
       </View>
-    </View>
+    </Pressable>
   )
 }
 
