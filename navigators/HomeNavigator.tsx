52515f4257100f86e074019ccbefcc54f073bf3b
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-08-06 16:23:01 +0700

    Vy fixed loading & error indicator

--- navigators/HomeNavigator.tsx
+++ navigators/HomeNavigator.tsx
@@ -1,40 +1,37 @@
-import { createStackNavigator } from '@react-navigation/stack'
+import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
 import HomeScreen from '../screens/HomeScreen'
 import SearchScreen from '../screens/SearchScreen'
 import BillboardDetailScreen from '../screens/BillboardDetailScreen'
 import SeeMoreBillboardListScreen from '../screens/SeeMoreBillboardListScreen'
-import { TouchableOpacity, View } from 'react-native'
-import Ionicons from '@expo/vector-icons/Ionicons'
-import { useNavigation } from '@react-navigation/native'
-import Colors from '../constants/color'
 import ProfileScreen from '../screens/ProfileScreen'
+
 const HomeStack = createStackNavigator()
 
 const HomeNavigator = () => {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
-      <HomeStack.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{ headerShown: false }}
-      />
-      <HomeStack.Screen
-        name="SeeMoreBillboardListScreen"
-        component={SeeMoreBillboardListScreen}
-      />
-      <HomeStack.Screen
-        name="BillboardDetailScreen"
-        component={BillboardDetailScreen}
-        options={{
-          headerTransparent: true,
-          headerTitle: '',
-        }}
-      />
-      <HomeStack.Screen name="Profile" component={ProfileScreen} />
-    </HomeStack.Navigator>
-  )
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="HomeScreen" component={HomeScreen}/>
+            <HomeStack.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{headerShown: false}}
+            />
+            <HomeStack.Screen
+                name="SeeMoreBillboardListScreen"
+                component={SeeMoreBillboardListScreen}
+            />
+            <HomeStack.Screen
+                name="BillboardDetailScreen"
+                component={BillboardDetailScreen}
+                options={{
+                    headerTransparent: true,
+                    headerTitle: '',
+                }}
+            />
+            <HomeStack.Screen name="Profile" component={ProfileScreen}/>
+        </HomeStack.Navigator>
+    )
 }
 
 export default HomeNavigator
