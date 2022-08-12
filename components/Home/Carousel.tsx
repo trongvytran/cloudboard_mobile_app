e5ab4ad3c7283fa5a49865fd05b11b0a5d2844a4
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-08-12 12:14:09 +0700

    Vy final rebase and goodbye to Cloudboard

--- components/Home/Carousel.tsx
+++ components/Home/Carousel.tsx
@@ -1,26 +1,26 @@
-import { View, FlatList } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, FlatList, Pressable} from 'react-native'
 import CarouselItem from '../Home/CarouselItem'
-import React from 'react'
 
-const Carousel: React.FC = ({ data }: any) => {
-  return (
-    <View className="px-4">
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        horizontal
-        scrollEnabled
-        snapToAlignment="center"
-        scrollEventThrottle={16}
-        decelerationRate={'fast'}
-        showsHorizontalScrollIndicator={false}
-        ItemSeparatorComponent={() => <View className="w-6" />}
-        renderItem={({ item: item }) => {
-          return <CarouselItem item={item} />
-        }}
-      />
-    </View>
-  )
+const Carousel = ({data}: any) => {
+    return (
+        <View className="px-4">
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                horizontal
+                scrollEnabled
+                snapToAlignment="center"
+                scrollEventThrottle={1}
+                decelerationRate={'fast'}
+                showsHorizontalScrollIndicator={false}
+                ItemSeparatorComponent={() => <View className="w-4"/>}
+                renderItem={({item: item}) => {
+                    return <CarouselItem item={item}/>
+                }}
+            />
+        </View>
+    )
 }
 
 export default Carousel
--- components/Profile/BillboardInput.tsx
+++ components/Profile/BillboardInput.tsx
@@ -1,110 +1,117 @@
-import { View, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native'
-import { useState } from 'react'
-import { styled } from 'nativewind'
+import {View, Text, TextInput, ScrollView, TouchableOpacity} from 'react-native'
+import React, {useState} from 'react'
+import {styled} from 'nativewind'
+import {LinearGradient} from "expo-linear-gradient";
+import {Animated} from "react-native";
+import Colors from "../../constants/color"
 
 const BillboardInput = () => {
-  const [title, setTitle] = useState('')
-  const [address, setAddress] = useState('')
-  const [startingPrice, setStartingPrice] = useState('')
-  const [description, setDescription] = useState('')
-  const [duration, setDuration] = useState('')
-  const [district, setDistrict] = useState('')
-  const [city, setCity] = useState('')
-  const [borderRadius, setBorderRadius] = useState(Number)
-  const StyledText = styled(Text)
-  const StyledTextInput = styled(TextInput)
-  const StyledTouchableOpacity = styled(TouchableOpacity)
+    const [title, setTitle] = useState('')
+    const [address, setAddress] = useState('')
+    const [startingPrice, setStartingPrice] = useState('')
+    const [description, setDescription] = useState('')
+    const [duration, setDuration] = useState('')
+    const [district, setDistrict] = useState('')
+    const [city, setCity] = useState('')
+    const [borderRadius, setBorderRadius] = useState(Number)
+    const StyledText = styled(Text)
+    const StyledTextInput = styled(TextInput)
+    const StyledTouchableOpacity = styled(TouchableOpacity)
 
-  return (
-    <ScrollView>
-      <View className={`border w-full rounded-lg p-10 mb-4`}>
-        <StyledText className={`text-center font-bold`}>
-          Add your Billboard
-        </StyledText>
-        <StyledText className={`text-center italic`}>
-          New Billboard, More Income!
-        </StyledText>
-      </View>
-      <View>
-        <View>
-          <StyledText>Title:</StyledText>
-          {/* // Billboard Name */}
-          <StyledTextInput
-            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-          />
-        </View>
-        {/* // Billboard Address */}
-        <View>
-          <StyledText>Address:</StyledText>
-          <StyledTextInput
-            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-          />
-        </View>
-        {/* // Billboard Price Range (low) */}
-        <View className={`flex flex-1 flex-row justify-between items-center`}>
-          <View className={`flex flex-1 mr-2.5`}>
-            <StyledText>Starting Price:</StyledText>
-            <StyledTextInput
-              className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-              keyboardType="numeric"
-            />
-          </View>
-          {/* // Billboard Price Range (high) */}
-          <View className={`flex flex-1`}>
-            <StyledText>Highest Price:</StyledText>
-            <StyledTextInput
-              className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-              keyboardType="numeric"
-            />
-          </View>
-        </View>
-        {/* // Billboard Description */}
-        <View>
-          <StyledText>Description:</StyledText>
-          <StyledTextInput
-            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-            multiline={true}
-          />
-        </View>
-        {/* // Billboard Rent Duration */}
-        <View>
-          <StyledText>Duration:</StyledText>
-          <StyledTextInput
-            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-          />
-        </View>
-        <View className={`flex flex-1 flex-row justify-between items-center`}>
-          <View className={`flex flex-1 mr-2.5`}>
-            <StyledText>District:</StyledText>
-            {/* // Billboard's District */}
-            <StyledTextInput
-              className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-            />
-          </View>
-          {/* // Billboard's City */}
-          <View className={`flex flex-1`}>
-            <StyledText>City:</StyledText>
-            <StyledTextInput
-              className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-            />
-          </View>
-        </View>
-        {/* // Random */}
-        <View>
-          <StyledText>Pending</StyledText>
-          <StyledTextInput
-            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
-          />
-        </View>
-        <StyledTouchableOpacity
-          className={`w-full rounded-md bg-red-600 py-3 px-4 mt-2`}
-        >
-          <StyledText className={`text-white font-bold text-center`}>
-            SUBMIT
-          </StyledText>
-        </StyledTouchableOpacity>
-      </View>
-    </ScrollView>
-  )
+    const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient)
+
+    return (
+        <ScrollView>
+            <AnimatedLinearGradient style={{padding: 40, marginBottom: 16, borderRadius: 8}}
+                                    colors={[Colors.primaryColor, "rgba(255, 138, 0, 1)"]}
+                                    start={[0, 0]} end={[1, 1]}>
+                <StyledText className={`text-gray-100 text-xl text-center font-bold`}>
+                    Add your Billboard
+                </StyledText>
+                <StyledText className={`text-gray-100 text-lg text-center italic`}>
+                    New Billboard, More Income!
+                </StyledText>
+            </AnimatedLinearGradient>
+            <View>
+                <View>
+                    <StyledText>Title:</StyledText>
+                    {/* // Billboard Name */}
+                    <StyledTextInput
+                        className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                    />
+                </View>
+                {/* // Billboard Address */}
+                <View>
+                    <StyledText>Address:</StyledText>
+                    <StyledTextInput
+                        className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                    />
+                </View>
+                {/* // Billboard Price Range (low) */}
+                <View className={`flex flex-1 flex-row justify-between items-center`}>
+                    <View className={`flex flex-1 mr-2.5`}>
+                        <StyledText>Starting Price:</StyledText>
+                        <StyledTextInput
+                            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                            keyboardType="numeric"
+                        />
+                    </View>
+                    {/* // Billboard Price Range (high) */}
+                    <View className={`flex flex-1`}>
+                        <StyledText>Highest Price:</StyledText>
+                        <StyledTextInput
+                            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                            keyboardType="numeric"
+                        />
+                    </View>
+                </View>
+                {/* // Billboard Description */}
+                <View>
+                    <StyledText>Description:</StyledText>
+                    <StyledTextInput
+                        className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                        multiline={true}
+                    />
+                </View>
+                {/* // Billboard Rent Duration */}
+                <View>
+                    <StyledText>Duration:</StyledText>
+                    <StyledTextInput
+                        className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                    />
+                </View>
+                <View className={`flex flex-1 flex-row justify-between items-center`}>
+                    <View className={`flex flex-1 mr-2.5`}>
+                        <StyledText>District:</StyledText>
+                        {/* // Billboard's District */}
+                        <StyledTextInput
+                            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                        />
+                    </View>
+                    {/* // Billboard's City */}
+                    <View className={`flex flex-1`}>
+                        <StyledText>City:</StyledText>
+                        <StyledTextInput
+                            className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                        />
+                    </View>
+                </View>
+                {/* // Random */}
+                <View>
+                    <StyledText>Pending</StyledText>
+                    <StyledTextInput
+                        className={`max-h-11 p-2.5 my-2.5 bg-white rounded w-full border`}
+                    />
+                </View>
+                <StyledTouchableOpacity
+                    className={`w-full rounded-md bg-red-600 py-3 px-4 mt-2`}
+                >
+                    <StyledText className={`text-white font-bold text-center`}>
+                        SUBMIT
+                    </StyledText>
+                </StyledTouchableOpacity>
+            </View>
+        </ScrollView>
+    )
 }
 export default BillboardInput
