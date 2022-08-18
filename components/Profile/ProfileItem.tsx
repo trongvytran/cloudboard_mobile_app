8af7560b64d101cc70179977f2836417e75ffd8f
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-08-18 13:48:02 +0700

    Vy added some new input fields and optimised UI

--- components/Profile/ProfileItem.tsx
+++ components/Profile/ProfileItem.tsx
@@ -4,45 +4,47 @@ import Colors from '../../constants/color'
 import Ionicons from '@expo/vector-icons/Ionicons'
 
 interface ProfileItemProps {
-  title: string
-  subtitle: string,
-  onPress: () => void
+    title: string
+    subtitle: string,
+    onPress: () => void
 }
 
-const ProfileItem = ({ title, subtitle, onPress }: ProfileItemProps) => {
-  return (
-    <Pressable onPress={onPress} style={styles.container}>
-      <View>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.subtitle}>{subtitle}</Text>
-      </View>
-      <Ionicons style={styles.icon} name="ios-chevron-forward" size={20} />
-    </Pressable>
-  )
+const ProfileItem = ({icon, title, subtitle, onPress}: ProfileItemProps) => {
+    return (
+        <Pressable onPress={onPress} style={styles.container}>
+            <View className={'flex flex-row'}>
+                <Ionicons name={icon} size={28} color={Colors.textColor}/>
+                <Text className={'flex py-1 pl-3'}
+                      style={styles.title}>{title}</Text>
+                {/*<Text style={styles.subtitle}>{subtitle}</Text>*/}
+            </View>
+            <Ionicons style={styles.icon} name="ios-chevron-forward" size={20}/>
+        </Pressable>
+    )
 }
 
 export default ProfileItem
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: Colors.borderColor,
-    paddingBottom: 17,
-    paddingTop: 17,
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: '600',
-    marginBottom: 5,
-  },
-  subtitle: {
-    fontSize: 11,
-    color: Colors.textColor,
-  },
-  icon: {
-    color: Colors.textColor,
-  },
+    container: {
+        flexDirection: 'row',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderColor: Colors.borderColor,
+        paddingBottom: 17,
+        paddingTop: 17,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    subtitle: {
+        fontSize: 11,
+        color: Colors.textColor,
+    },
+    icon: {
+        color: Colors.textColor,
+    },
 })
--- screens/PaymentMethodScreen.tsx
+++ screens/PaymentMethodScreen.tsx
@@ -1,22 +1,51 @@
-import { StyleSheet, Text, ScrollView } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, ScrollView, View} from 'react-native'
+import {CardField, CardForm, useStripe} from '@stripe/stripe-react-native';
+import React, {useState} from 'react'
 import ContainerView from '../components/Views/ContainerView'
 import Colors from '../constants/color'
 
 const PaymentMethodScreen = () => {
-  return (
-    <ScrollView style={styles.container}>
-      <ContainerView>
-        <Text>PaymentMethodScreen</Text>
-      </ContainerView>
-    </ScrollView>
-  )
+    const [cardDetails, setCardDetails] = useState('')
+    const {confirmPayment} = useStripe();
+    return (
+        <ScrollView style={styles.container}>
+            <Text className={`mx-4 mt-3 text-lg font-bold`}>Add new methods</Text>
+            <View className={`px-4`}>
+                <CardField
+                    postalCodeEnabled={false}
+                    placeholder={{
+                        number: '1234 5678 9764 9999',
+                    }}
+                    cardStyle={{
+                        backgroundColor: '#FFFFFF',
+                        borderColor: '#000000',
+                        borderWidth: 1,
+                        textColor: '#000000',
+                        textErrorColor: '#f81111'
+                    }}
+                    style={{
+                        width: '100%',
+                        height: 50,
+                        marginVertical: 10,
+                    }}
+                    onCardChange={(cardDetails) => {
+                        setCardDetails(cardDetails)
+                    }}
+                    onFocus={(focusedField) => {
+                        console.log('focusField', focusedField);
+                    }}
+                />
+            </View>
+            <Text className={`mx-4 mt-3 text-lg font-bold`}>Existing methods</Text>
+            
+        </ScrollView>
+    )
 }
 
 export default PaymentMethodScreen
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.layoutColor,
-  },
+    container: {
+        backgroundColor: Colors.layoutColor,
+    },
 })
