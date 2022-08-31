aa3b0cc03c6840b46220ee92d30326684a60e987
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-08-31 20:35:00 +0700

    Vy finished Contact Screen

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,28 +1,64 @@
 import React from 'react'
 import {View, Linking, Text, TouchableOpacity} from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const makeACall = (telephone: string) => {
     Linking.openURL(`tel:${telephone}`)
         .then(res => console.log('Calling...'))
 }
+
+const sendAnEmail = (email: string) => {
+    Linking.openURL(`mailto:${email}`)
+        .then(res => console.log('Opening...'))
+}
 const Contact = () => {
     return (
-        <View className={'flex flex-row bg-white p-4'}>
+        <View>
             <View>
-                <Text className={'mx-auto py-4 font-bold'}>
-                    Tran Trong Vy
-                </Text>
+                <Text className={'font-bold mt-2 ml-4 text-lg'}>Phone</Text>
             </View>
-            <View className={'mx-auto p-4 bg-green-500'}>
-                <TouchableOpacity className={' bg-green-500'} onPress={() => {
-                    makeACall('+84907604950')
-                }}><Text>
-                    0907604950
-                </Text>
-                </TouchableOpacity>
+            <View className={'flex flex-row justify-center items-center my-2'}>
+                {/*<View className={'flex mx-4 justify-center items-center'}>*/}
+                {/*    <Text className={'font-bold text-lg p-4'}>*/}
+                {/*        Tran Trong Vy*/}
+                {/*    </Text>*/}
+                {/*</View>*/}
+                <View className={'p-4 mx-4 bg-green-500 rounded-xl'}>
+                    <TouchableOpacity
+                        className={'flex flex-row align-middle justify-center items-center bg-green-500'}
+                        onPress={() => {
+                            makeACall('+84907604950')
+                        }}>
+                        <Ionicons name={'call'} color={'white'} size={24}/>
+                        <Text className={'font-bold text-white align-middle ml-2 text-lg'}>
+                            0907604950
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View>
+                <Text className={'font-bold mt-2 ml-4 text-lg'}>Email</Text>
+            </View>
+            <View className={'flex flex-row justify-center items-center my-2'}>
+                {/*<View className={'flex mx-4 justify-center items-center'}>*/}
+                {/*    <Text className={'font-bold text-lg p-4'}>*/}
+                {/*        Tran Trong Vy*/}
+                {/*    </Text>*/}
+                {/*</View>*/}
+                <View className={'px-4 py-5 mx-4 bg-blue-500 rounded-xl'}>
+                    <TouchableOpacity
+                        className={'flex flex-row align-middle justify-center items-center'}
+                        onPress={() => {
+                            sendAnEmail('john.appleseed@icloud.com')
+                        }}>
+                        <Ionicons name={'mail'} color={'white'} size={24}/>
+                        <Text className={'font-bold text-white align-middle ml-2 text-lg'}>
+                            john.appleseed@icloud.com
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
-
     )
 }
 export default Contact
