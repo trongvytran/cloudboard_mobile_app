15ab9198644d00648dd4ea4a949d5579dc56c5ef
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-09-02 18:12:55 +0700

    Vy unified Token to prevent duplication

--- screens/SubscriptionScreen.tsx
+++ screens/SubscriptionScreen.tsx
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import ContainerView from '../components/Views/ContainerView'
 import Subscription from '../components/Home/Subscription'
 import axios from 'axios'
-import { useSelector } from 'react-redux'
+import {useSelector} from 'react-redux'
 import TitleText from '../components/TitleText'
 import LayoutView from '../components/Views/LayoutView'
 import BaseUrl from '../constants/baseUrl'
 
 const SubscriptionScreen = () => {
-  const [data, setData] = useState([])
-  const { userLoginInfo } = useSelector((state: any) => state.userLoginInfo)
-  const { userToken } = useSelector((state: any) => state.userToken)
+    const [data, setData] = useState([])
+    const {userLoginInfo} = useSelector((state: any) => state.userLoginInfo)
 
-  useEffect(() => {
-    if (userLoginInfo != null) {
-      axios
-        .get(`${BaseUrl}/api/users/${userLoginInfo.id}`, {
-          headers: { Authorization: `Bearer ${userToken}` },
-        })
-        .then((res) => {
-          setData(res.data)
-          console.log(data);
-        })
-    }
-  }, [])
+    useEffect(() => {
+        if (userLoginInfo != null) {
+            axios
+                .get(`${BaseUrl}/api/users/${userLoginInfo.id}`, {
+                    headers: {Authorization: `Bearer ${userLoginInfo}`},
+                })
+                .then((res) => {
+                    setData(res.data)
+                    console.log(data);
+                })
+        }
+    }, [])
 
-  return (
-    <LayoutView>
-      <ContainerView>
-        <TitleText>Subscriptions</TitleText>
-        <Subscription data={(data as any).subscriptions} />
-      </ContainerView>
-    </LayoutView>
-  )
+    return (
+        <LayoutView>
+            <ContainerView>
+                <TitleText>Subscriptions</TitleText>
+                <Subscription data={(data as any).subscriptions}/>
+            </ContainerView>
+        </LayoutView>
+    )
 }
 
 export default SubscriptionScreen
