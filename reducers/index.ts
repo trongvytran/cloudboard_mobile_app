30b35c867b159e1ab6665bfc8a83995ba81a9fa3
minh <minhphamhuy811@gmail.com>
2022-09-07 23:05:31 +0700

    Vy final rebase and goodbye to Cloudboard

--- reducers/index.ts
+++ reducers/index.ts
@@ -2,9 +2,10 @@ import {combineReducers} from 'redux'
 import userLoginInfoReducer from '../features/auth/userLoginInfo'
 import shareBillboardInfoReducer from '../features/shareBillboardInfo'
 // import likeBillboardInfoReducer from '../features/likeBillboardInfo'
-
+import userCredicardReducer from '../features/userCreditCard'
 export default combineReducers({
     userLoginInfo: userLoginInfoReducer,
     shareBillboardInfo: shareBillboardInfoReducer,
-    // likeBillboardInfo: likeBillboardInfoReducer
+    // likeBillboardInfo: likeBillboardInfoReducer  
+    userCredicard: userCredicardReducer
 })
