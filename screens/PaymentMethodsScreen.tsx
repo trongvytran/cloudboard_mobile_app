3cf4bbb4df17da0d28b3f0ef100a64c678dace6f
minh <minhphamhuy811@gmail.com>
2022-09-09 03:42:25 +0700

    Vy final rebase and goodbye to Cloudboard

--- screens/PaymentMethodsScreen.tsx
+++ screens/PaymentMethodsScreen.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, ScrollView, View, TouchableOpacity, Linking, Alert} from 'react-native'
 import {CardField, useStripe} from '@stripe/stripe-react-native';
-import React, {useCallback, useEffect, useLayoutEffect, useRef, useState} from 'react'
+import React, {useEffect, useLayoutEffect, useRef, useState} from 'react'
 import Colors from '../constants/color'
 import {useNavigation} from "@react-navigation/native";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -8,12 +8,10 @@ import axios from 'axios';
 import baseUrl from "../constants/baseUrl";
 import {useDispatch, useSelector} from "react-redux";
 import {addUserCreditCard} from '../features/userCreditCard'
-import {PaymentIcon} from 'react-native-payment-icons'
 
 const PaymentMethodsScreen = () => {
     const navigation = useNavigation()
     const {userLoginInfo} = useSelector((state: any) => state.userLoginInfo)
-    const {userCreditCard} = useSelector((state: any) => state.userCreditCard)
     const stripe = useStripe();
     const stripeCustomerId = userLoginInfo.stripeCustomerId
     const dispatch = useDispatch()
@@ -34,6 +32,9 @@ const PaymentMethodsScreen = () => {
         })
     )
 
+    useEffect(() => {
+    })
+
     const getPaymentMethodId = async () => {
         const stripeResponse = await stripe?.createPaymentMethod({
             type: 'Card',
@@ -45,18 +46,19 @@ const PaymentMethodsScreen = () => {
         return paymentMethod.id
     }
 
-    // const manage = async () => {
-    //     await axios.post(`${baseUrl}/api/transactions/portal`, {
-    //             stripeCustomerId
-    //         }
-    //     )
-    //         .then((res) => {
-    //             Linking.openURL(res.data.url).catch(err => console.error("Couldn't load page", err));
-    //         })
-    //
-    // }
+   
 
-    const postCreditCard = useCallback(async () => {
+    const manage = async () => {
+        await axios.post(`${baseUrl}/api/transactions/portal`, {
+                stripeCustomerId
+            }
+        )
+            .then((res) => {
+                Linking.openURL(res.data.url).catch(err => console.error("Couldn't load page", err));
+            })
+
+    }
+    const postCreditCard = async () => {
         const paymentMethodId = await getPaymentMethodId();
 
         if (!paymentMethodId) {
@@ -67,36 +69,37 @@ const PaymentMethodsScreen = () => {
                     paymentMethodId,
                     stripeCustomerId
                 }
-            ).then(() => Alert.alert('Payment added successfully!')).then()
+            ).then(() => Alert.alert('Payment added successfully!'))
         } catch (e) {
             throw new Error(`Error occured: ${e}`)
         }
-    }, [userCreditCard])
+    }
 
     const setDefault = async (paymentMethodId) => {
         await axios.post(`${baseUrl}/api/transactions/default`, {
-                paymentMethodId,
-                stripeCustomerId
-            }
-        ).then(res => {
-                console.log(res.data)
-            }
+            paymentMethodId,
+            stripeCustomerId
+            
+        }
+        ).then(res =>{console.log(res.data)}
         )
 
     }
-    const emptyPaymentMethod = () => {
-        return (<View>
-            <Text>No Payment Method Found.</Text>
-        </View>)
+
+    const getPaymentMethods = async () => {
+        const stripeCustomerId = userLoginInfo.stripeCustomerId
+        await axios.get(`${baseUrl}/api/transactions/credit-cards/${stripeCustomerId}`,
+        ).then((res) => setUserCreditCards(res.data.data))
     }
 
+
+    const [cardDetails, setCardDetails] = useState([])
+    const [userCreditCard, setUserCreditCards] = useState([])
+
     useEffect(() => {
-        const getPaymentMethods = async () => {
-            await axios.get(`${baseUrl}/api/transactions/credit-cards/${stripeCustomerId}`,
-            ).then((res) => dispatch(addUserCreditCard(res.data.data)))
-        }
-        getPaymentMethods().catch(console.error)
-    }, [])
+        getPaymentMethods()
+   
+    })
 
     return (
         <ScrollView style={styles.container}>
@@ -119,38 +122,62 @@ const PaymentMethodsScreen = () => {
                         height: 50,
                         marginVertical: 10,
                     }}
+                    onCardChange={
+                        setCardDetails
+                    }
                 />
             </View>
             <View className={'flex flex-row justify-center mx-5 mt-0.5'}>
-                {/*<TouchableOpacity className={'px-2 py-3.5 w-1/2 mr-2 bg-blue-600 rounded-lg'}*/}
-                {/*                  onPress={() => manage()}>*/}
-                {/*    <Text className={'text-white align-middle text-center font-bold text-lg'}>MANAGE</Text>*/}
-                {/*</TouchableOpacity>*/}
-                <TouchableOpacity className={'px-2 py-3.5 w-full bg-red-600 rounded-lg'}
+                <TouchableOpacity className={'px-2 py-3.5 w-1/2 mr-2 bg-blue-600 rounded-lg'}
+                                  onPress={() => manage()}>
+                    <Text className={'text-white align-middle text-center font-bold text-lg'}>MANAGE</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className={'px-2 py-3.5 w-1/2 bg-red-600 rounded-lg'}
                                   onPress={() => postCreditCard()}>
                     <Text className={'text-white align-middle text-center font-bold text-lg'}>SUBMIT</Text>
                 </TouchableOpacity>
             </View>
             <View>
                 <Text className={`mx-4 mt-3 text-lg font-bold`}>Existing methods</Text>
-                {userCreditCard > 0 && userCreditCard.map((index, i) => {
-                        return (
-                            <View key={index}>
-                                <PaymentIcon type={userCreditCard[i].card.brand}></PaymentIcon>
-                                <Text>Brand: {userCreditCard[i].card.brand}</Text>
-                                <Text>Last 4: {userCreditCard[i].card.last4}</Text>
-                                <Text>Expire Month : {userCreditCard[i].card.exp_month}</Text>
-                                <Text>Expire Year : {userCreditCard[i].card.exp_year}</Text>
-                                <TouchableOpacity className={'px-2 py-3.5 w-1/2 bg-red-600 rounded-lg'}
-                                                  onPress={() => setDefault(userCreditCard[i].id)}>
-                                    <Text className={'text-white align-middle text-center font-bold text-lg'}>Set as
-                                        default</Text>
-                                </TouchableOpacity>
 
-                            </View>
+                        {userCreditCard.map(( index,i) => {
+                        return (
+                    <>
+                    <Text>Card {i+1}</Text>
+                    <Text>Brand: {userCreditCard[i].card.brand}</Text>
+                    <Text>Last 4: {userCreditCard[i].card.last4}</Text>
+                    <Text>Expire Month : {userCreditCard[i].card.exp_month}</Text>
+                    <Text>Expire Year : {userCreditCard[i].card.exp_year}</Text>
+                    <Text>Expire Year : {userCreditCard[i].id}</Text>
+                    <TouchableOpacity className={'px-2 py-3.5 w-1/2 bg-red-600 rounded-lg'}
+                                  onPress={() => setDefault(userCreditCard[i].id)}>
+                    <Text className={'text-white align-middle text-center font-bold text-lg'}>Set as default</Text>
+                    </TouchableOpacity>
+
+                    </>
+                    //     <CardField  key={i}
+                    //     postalCodeEnabled={false}
+                    //     placeholder={{
+                    //         number: '**** **** **** ****  '+ userCreditCard[i].card.last4,
+                    //         expiration:""+userCreditCard[i].card.exp_month + "/" + userCreditCard[i].card.exp_year
+                    //     }}
+                    //     cardStyle={{
+                    //         backgroundColor: '#FFFFFF',
+                    //         borderColor: '#000000',
+                    //         borderWidth: 1,
+                    //         textColor: '#000000',
+                    //         textErrorColor: '#f81111'
+                    //     }}
+                    //     style={{
+                    //         width: '100%',
+                    //         height: 50,
+                    //         marginVertical: 10,
+                    //     }}
+                    // />
                         )
-                    }
-                )}
+                       }
+                        )}
+
 
 
                 {/*<Text className={'mx-auto text-gray-400 my-10'}>{cardDetails}</Text>*/}
