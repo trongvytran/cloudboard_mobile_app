b5e206b673c97b113e8cb13826c4cfb36be6b88a
An Le1 <an_le1@epam.com>
2022-09-16 20:59:49 +0700

    Add book detail

--- components/Home/BillboardDetail.tsx
+++ components/Home/BillboardDetail.tsx
@@ -1,18 +1,26 @@
-import {View, Text, Image, TouchableOpacity, Alert, ScrollView, Linking} from 'react-native'
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    Alert,
+    ScrollView,
+    GestureResponderEvent,
+} from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons'
-import MapView, {Marker} from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import DurationBadge from '../UI/DurationBadge'
-import React, {useLayoutEffect} from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import ReadMore from 'react-native-read-more-text'
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 import ShareButton from '../UI/ShareButton'
-import {useSelector} from 'react-redux'
-import{ BASE_URL } from "../../constants/endpoints";
-import axios from "axios";
-import moment from "moment";
-const BillboardDetail = ({item}: any) => {
+import { useSelector } from 'react-redux'
+import axios from 'axios'
+import { BASE_URL } from '../../constants/endpoints'
+const BillboardDetail = ({ item }: any) => {
     const navigation = useNavigation()
-    const {userLoginInfo} = useSelector((state: any) => state.userLoginInfo)
+    const [booking, setBooking] = useState('Book')
+    const { userLoginInfo } = useSelector((state: any) => state.userLoginInfo)
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -21,17 +29,42 @@ const BillboardDetail = ({item}: any) => {
                     className="flex items-center justify-center w-12 h-12 mt-4 ml-4 rounded-full bg-gray-900/60"
                     onPress={() => navigation.navigate('HomeScreen' as never)}
                 >
-                    <Ionicons name="arrow-back-outline" size={24} color="white"/>
+                    <Ionicons
+                        name="arrow-back-outline"
+                        size={24}
+                        color="white"
+                    />
                 </TouchableOpacity>
             ),
             headerRight: () => (
                 <View className="flex flex-row items-center mt-4 mr-4">
-                    <ShareButton value={item}/>
+                    <ShareButton value={item} />
                 </View>
             ),
-
         })
     }, [navigation])
+
+    useLayoutEffect(() => {
+        axios.get(`${BASE_URL}/api/users/${userLoginInfo.id}`).then((res) => {
+            const val = res.data.bookedBillboards.find(
+                (element: { id: any }) => {
+                    element.id === item.id
+                }
+            )
+            if (val) {
+                setBooking('Booked')
+            }
+        })
+    }, [])
+
+    const bookBillboard = async () => {
+        await axios.patch(
+            `${BASE_URL}/api/users/${userLoginInfo.id}/book-billboard`,
+            { billboardId: item.id }
+        )
+        Alert.alert('Booked sucessfully')
+        setBooking('Booked')
+    }
 
     const handleContact = () => {
         if (!userLoginInfo) {
@@ -46,28 +79,36 @@ const BillboardDetail = ({item}: any) => {
                     {
                         text: 'OK',
                         onPress: () => {
-                            navigation.navigate('My Profile' as never, {screen: 'ProfileScreen'} as never)
+                            navigation.navigate(
+                                'My Profile' as never,
+                                { screen: 'ProfileScreen' } as never
+                            )
                         },
                     },
                 ]
             )
         } else {
-            navigation.navigate('ContactScreen' as never, {item} as never)
+            navigation.navigate('ContactScreen' as never, { item } as never)
         }
     }
 
     return (
         <ScrollView>
-            <Image className="w-full aspect-[4/3]" source={{uri: item.imageUrl}}/>
+            <Image
+                className="w-full aspect-[4/3]"
+                source={{ uri: item.imageUrl }}
+            />
             <View className="p-4">
                 <View className="flex flex-row items-center justify-between mb-2">
                     <Text className="text-xl font-bold tracking-tight text-gray-900">
                         {item.name}
                     </Text>
-                    <DurationBadge value={item.duration}/>
+                    <DurationBadge value={item.duration} />
                 </View>
                 <Text className="mb-2 text-gray-700">{item.address}</Text>
-                <Text className="text-lg font-medium text-amber-500">{item.price}</Text>
+                <Text className="text-lg font-medium text-amber-500">
+                    {item.price}
+                </Text>
                 <Text className="my-2 text-lg font-bold">Description</Text>
                 <ReadMore
                     numberOfLines={2}
@@ -80,15 +121,19 @@ const BillboardDetail = ({item}: any) => {
                     <View className="flex flex-row items-center">
                         <Image
                             className="w-10 h-10 rounded-full"
-                            source={{uri: item.user.imageUrl}}
+                            source={{ uri: item.user.imageUrl }}
                         />
-                        <Text className="ml-3 font-medium">{item.user.name}</Text>
+                        <Text className="ml-3 font-medium">
+                            {item.user.name}
+                        </Text>
                     </View>
                     <TouchableOpacity
                         onPress={handleContact}
                         className="px-4 py-2 font-medium border-2 rounded-lg border-amber-500"
                     >
-                        <Text className="font-medium text-amber-500">CONTACT</Text>
+                        <Text className="font-medium text-amber-500">
+                            CONTACT
+                        </Text>
                     </TouchableOpacity>
                 </View>
                 <View className="w-full aspect-[4/3] rounded-lg shadow">
@@ -101,19 +146,36 @@ const BillboardDetail = ({item}: any) => {
                         }}
                         className="w-full h-full rounded-lg"
                     >
-                        <Marker coordinate={{latitude: item.lat, longitude: item.long}}/>
+                        <Marker
+                            coordinate={{
+                                latitude: item.lat,
+                                longitude: item.long,
+                            }}
+                        />
                     </MapView>
                 </View>
-                <TouchableOpacity className={'px-2 py-3.5 w-1/2 mr-2 bg-blue-600 rounded-lg'}
-                                  onPress={() => Subscribe()}>
-                    <Text className={'text-white align-middle text-center font-bold text-lg'}>Subscribe</Text>
+                <TouchableOpacity
+                    onPress={bookBillboard}
+                    className={
+                        'px-2 py-2.5 w-full mr-2 bg-amber-500 rounded-lg my-3'
+                    }
+                >
+                    <Text
+                        className={
+                            'text-white align-middle text-center font-bold text-lg'
+                        }
+                    >
+                        {booking}
+                    </Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>
     )
 }
 
-const _renderTruncatedFooter = (handlePress) => {
+const _renderTruncatedFooter = (
+    handlePress: (event: GestureResponderEvent) => void
+) => {
     return (
         <View className="items-center justify-center pt-4">
             <Text className="text-amber-500" onPress={handlePress}>
@@ -123,7 +185,9 @@ const _renderTruncatedFooter = (handlePress) => {
     )
 }
 
-const _renderRevealedFooter = (handlePress) => {
+const _renderRevealedFooter = (
+    handlePress: (event: GestureResponderEvent) => void
+) => {
     return (
         <View className="items-center justify-center pt-4">
             <Text className="text-amber-500" onPress={handlePress}>
